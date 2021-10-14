81d9f56f4da6e829b3baa965a6d1a751e3fa7dcc
iagoffdz <iagofdez96@gmail.com>
2021-10-14 10:40:51 +0200

    Dos primeros ejemplos

---
+++ 02-Holausuario/src/com/liceolapaz/des/iff/principal.java
@@ -0,0 +1,39 @@
+package com.liceolapaz.des.iff;
+
+import java.util.Scanner;
+
+public class principal {
+
+	public static void main(String[] args) {
+		// Pedirle nombre al usuario
+		pedirNombre();
+		// Recibir el nombre y almacenarlo
+		String nombreUsuario = leerNombre();
+		// Escribir el saludo por pantalla
+        escribirSaludo();
+	escribirSaludo()
+	} 
+
+	
+	
+	private static void escribirSaludo(string nombreUsuario) {
+		System.out.println(“¡Hola!” + nombreUsuario);
+		
+	}
+
+
+
+	private static String leerNombre() {
+		Scanner escaner = new Scanner(System.in)
+		return escaner.nextLine();
+	}
+
+
+
+	private static void pedirNombre() {
+		System.out.println(“Escriba su nombre:”);
+		
+		
+	}
+
+}
